08c503c990758c7941ade60c753e99a1dcc9515c
Leo Flores <leodflowers@gmail.com>
2025-09-30 15:22:21 -0400

    add basic grower page

--- ui/src/Grower.tsx
+++ ui/src/Grower.tsx
@@ -1,9 +1,18 @@
 import { useParams } from 'react-router';
+import growerService from './services/growers';
 
 function Grower() {
     let { id } = useParams();
-    return (
-        <h1>Grower - {id}</h1>
+    const grower = growerService.getById(id);
+
+    return ( 
+        <>
+            <h1>{grower.name}</h1>
+            <img src={grower.image} />
+            <p>
+                Bio goes here...
+            </p>
+        </>
     );
 }
 
--- ui/src/services/growers.ts
+++ ui/src/services/growers.ts
@@ -1,14 +1,21 @@
 import Grower from "../entities/grower";
 
+const GROWERS = [
+    new Grower(1, "Evergreen Growers", 11237, "/images/farm1.jpg"),
+    new Grower(2, "Forever Farms", 11216, "/images/farm2.jpg"),
+    new Grower(3, "Red River Acres", 11301, "/images/farm3.jpg"),
+];
+
 // getAll: returns all harvests from source (currently mock data)
 function getAll(): Array<Grower> {
-    return [
-        new Grower(1, "Evergreen Growers", 11237, "/images/farm1.jpg"),
-        new Grower(2, "Forever Farms", 11216, "/images/farm2.jpg"),
-        new Grower(3, "Red River Acres", 11301, "/images/farm3.jpg"),
-    ]
+    return GROWERS;
 }
+
+function getById(id: string): Grower {
+    return GROWERS[id];
+} 
 
 export default {
     getAll: getAll,
+    getById: getById,
 }
