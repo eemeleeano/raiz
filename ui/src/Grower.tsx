90daac2cdf61b3379c124724012bb33abfda0e25
Leo Flores <leodflowers@gmail.com>
2025-10-08 08:29:03 -0400

    small refactor

--- ui/src/Grower.tsx
+++ ui/src/Grower.tsx
@@ -7,6 +7,15 @@ import harvestService from './services/harvests';
 // main page for growers. this is where they will have information about
 // their operation, images, bios, harvests available and maybe contact info.
 
+const Harvest = ({ id, name, imageLocation }) => {
+    return (
+        <a href={"/harvests/" + String(Number(id))}>
+            <img className="img-medium" src={imageLocation} />
+                {name}
+        </a>
+    );   
+}
+
 function Grower() {
     let { id } = useParams();
     const [grower, setGrower] = useState(null);
@@ -24,34 +33,33 @@ function Grower() {
             });
     }, []);
 
-    if (grower) {
-        return ( 
-            <>
-                <h1 className="grower-title">{grower.name}</h1>
-                <img src={grower.image} />
-                <p>
-                    Bio goes here...
-                </p>
-
-                <h2>Harvests</h2>
-                <ul>
-                    {harvests.map(h => 
-                        <li key={String(h.id)}>
-                            <a href={"/harvests/" + String(Number(h.id))}>
-                                <img className="img-medium" src={h.image} />
-                                {h.name}
-                            </a>
-                        </li>)}
-                </ul>
-            </>
-        );
-    } else {
+    if (!grower) {
         return (
             <>
                 <h1>Grower not found.</h1>
             </>
         );
     }
+
+    return ( 
+        <>
+            <h1 className="grower-title">{grower.name}</h1>
+            <img src={grower.image} />
+            <p>
+                Bio goes here...
+            </p>
+
+            <h2>Harvests</h2>
+            <ul>
+                {harvests.map(h => 
+                    <li key={String(h.id)}>
+                        <Harvest id={h.id} 
+                            name={h.name} 
+                            imageLocation={h.image} />
+                    </li>)}
+            </ul>
+        </>
+    );
 }
 
 export default Grower
--- ui/src/services/growers.ts
+++ ui/src/services/growers.ts
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import Grower from "../entities/grower";
 
+const instance = axios.create({
+    baseURL: 'http://localhost:9001',
+});
+
 // getAll: returns all harvests from source (currently mock data)
 async function getAll(): Promise<Array<Grower>> {
     var growers: Array<Grower> = [];
 
     try {
-        const response = await axios.get('http://localhost:9001/growers');
-        response.data.map((growerObject) => {
-            const newGrower = new Grower(
-                growerObject.id,
-                growerObject.name,
-                growerObject.location,
-                `/images/growers/farm${growerObject.id}.jpg`
-            );
-            growers.push(newGrower);
+        const response = await instance.get('/growers');
+        response.data.map((object) => {
+            growers.push(new Grower(
+                object.id,
+                object.name,
+                object.location,
+                `/images/growers/farm${object.id}.jpg`
+            ));
         });
     } catch (error) {
         console.log(error)
@@ -28,14 +31,14 @@ async function getById(id: string): Promise<Grower> {
     var grower: Grower = null;
 
     try {
-        const response = await axios.get(`http://localhost:9001/growers/${id}`);
-        const growerObject = response.data;
+        const response = await instance.get(`/growers/${id}`);
+        const object = response.data;
 
         grower = new Grower(
-            growerObject.id,
-            growerObject.name,
-            growerObject.location,
-            `/images/growers/farm${growerObject.id}.jpg`
+            object.id,
+            object.name,
+            object.location,
+            `/images/growers/farm${object.id}.jpg`
         );
     } catch (error) {
         console.log(error)
